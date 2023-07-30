580f3a6bc1d2473212ed9aa06c4b14df80733094
Yifei Kong <kong@yifei.me>
2023-07-30 20:29:35 +0800

    Change httpx to curl_cffi

--- pocket.py
+++ pocket.py
@@ -1,20 +1,20 @@
-import httpx
+from curl_cffi import requests
 from typing import List
 import argparse
 import pandas as pd
 from loguru import logger
-from yutils import normalize_url
+# from yutils import normalize_url
 
 CONSUMER_KEY = ""
 
 
-def authenticate():
+def authenticate(key: str):
     redirect_uri = "http://localhost/pocket"
     data = {
-        "consumer_key": CONSUMER_KEY,
+        "consumer_key": key,
         "redirect_uri": redirect_uri,
     }
-    rsp = httpx.post("https://getpocket.com/v3/oauth/request", data=data)
+    rsp = requests.post("https://getpocket.com/v3/oauth/request", data=data)
     code = rsp.text.replace("code=", "")
     req_url_temp = "https://getpocket.com/auth/authorize?request_token={code}&redirect_uri={redirect_uri}"
     req_url = req_url_temp.format(code=code, redirect_uri=redirect_uri)
@@ -22,8 +22,8 @@ def authenticate():
     # webbrowser.open(req_url, new=1)
     input("Press Enter to continue...")
 
-    authorize_data = {"consumer_key": CONSUMER_KEY, "code": code}
-    rsp = httpx.post(
+    authorize_data = {"consumer_key": key, "code": code}
+    rsp = requests.post(
         "https://getpocket.com/v3/oauth/authorize",
         json=authorize_data,
         headers={"X-Accept": "application/json"},
@@ -40,18 +40,21 @@ def retrive_list(access_token: str, state: str = "all") -> List[dict]:
         "detailType": "simple",
     }
     logger.info("Start to retrive the list")
-    rsp = httpx.get("https://getpocket.com/v3/get", params=params)
+    rsp = requests.get("https://getpocket.com/v3/get", params=params)
     post_list = []
     for post_json in rsp.json()["list"].values():
         url = post_json["given_url"]
-        url = normalize_url(url)
+        # url = normalize_url(url)
         title = post_json["given_title"] or post_json["resolved_title"]
         post_list.append({"url": url, "title": title})
     return post_list
 
 
 def main():
-    access_token = authenticate()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", help="Pocket access key to use.")
+    args = parser.parse_args()
+    access_token = authenticate(args.key)
     logger.info("Access token is {}", access_token)
     post_list = retrive_list(access_token)
     df = pd.DataFrame(post_list)
