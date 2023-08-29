4aad80b15045da52dbcb56ee4882844991082bb0
Yifei Kong <kong@yifei.me>
2023-08-29 16:31:41 +0800

    Update pocket download script

--- pocket.py
+++ pocket.py
@@ -3,9 +3,9 @@ from typing import List
 import argparse
 import pandas as pd
 from loguru import logger
-# from yutils import normalize_url
+import webbrowser
 
-CONSUMER_KEY = ""
+# from yutils import normalize_url
 
 
 def authenticate(key: str):
@@ -19,7 +19,7 @@ def authenticate(key: str):
     req_url_temp = "https://getpocket.com/auth/authorize?request_token={code}&redirect_uri={redirect_uri}"
     req_url = req_url_temp.format(code=code, redirect_uri=redirect_uri)
     logger.info("Open in your browser and click authorize: {}", req_url)
-    # webbrowser.open(req_url, new=1)
+    webbrowser.open(req_url, new=1)
     input("Press Enter to continue...")
 
     authorize_data = {"consumer_key": key, "code": code}
@@ -32,33 +32,57 @@ def authenticate(key: str):
     return rsp.json()["access_token"]
 
 
-def retrive_list(access_token: str, state: str = "all") -> List[dict]:
+def retrive_list(
+    key: str, access_token: str, state: str = "all", offset: int = 0
+) -> List[dict]:
     params = {
-        "consumer_key": CONSUMER_KEY,
+        "consumer_key": key,
         "access_token": access_token,
         "state": state,
         "detailType": "simple",
+        "offset": offset,
     }
-    logger.info("Start to retrive the list")
+    logger.info("Start to retrive the list, offset={}", offset)
     rsp = requests.get("https://getpocket.com/v3/get", params=params)
     post_list = []
+    print(rsp.json())
     for post_json in rsp.json()["list"].values():
         url = post_json["given_url"]
         # url = normalize_url(url)
         title = post_json["given_title"] or post_json["resolved_title"]
-        post_list.append({"url": url, "title": title})
+        status = "unread" if post_json["status"] == 0 else "archived"
+        post_list.append(
+            {
+                "pocket_id": post_json["item_id"],
+                "favorite": post_json["favorite"],
+                "status": status,
+                "url": url,
+                "title": title,
+            }
+        )
     return post_list
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--key", help="Pocket access key to use.")
+    parser.add_argument("--state", default="all", choices=("unread", "archive", "all"), help="Item state on Pocket")
     args = parser.parse_args()
     access_token = authenticate(args.key)
     logger.info("Access token is {}", access_token)
-    post_list = retrive_list(access_token)
-    df = pd.DataFrame(post_list)
-    df.to_csv("pocket.csv", index=False)
+    offset = 0
+    full_list = []
+    while True:
+        post_list = retrive_list(
+            args.key, access_token, state=args.state, offset=offset
+        )
+        full_list.extend(post_list)
+        logger.info("{} items found", len(post_list))
+        if len(post_list) < 1000:
+            break
+        offset += 1000
+    df = pd.DataFrame(full_list)
+    df.to_csv("pocket_%s.csv" % args.state, index=False)
 
 
 if __name__ == "__main__":
